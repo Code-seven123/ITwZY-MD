cad0519f8b1ac052c02070f1a019d6c67f63c0c2
Code-seven123 <manu024595@gmail.com>
2024-02-10 19:32:17 +0700

    fix stiker maker

--- index.js
+++ index.js
@@ -3,9 +3,6 @@ import fs from 'fs'
 import MAIN_LOGGER from './lib/logger.js'
 import process from 'process'
 import http from 'http'
-//import fsx from 'fs-extra'
-//const notCache = process.argv.includes('--not-cache')
-
 const logger = MAIN_LOGGER.child({})
 
 const online = await isOnline()
@@ -16,24 +13,19 @@ if(!(online)) {
   logger.info('You are currently online.')
 }
 
-/*if(!(notCache)){
-  if(!(fs.existsSync('./.cache'))){
-    fs.mkdir('./.cache', { recursive: true }, (err) => {
-	    if (err) {
-	      logger.error(err, 'Failed to create a folder.')
-	      process.exit()
-	    } else {
-	      logger.info('Folder "./.cache" berhasil dibuat.')
-	    }
-	  })
-  } else {
-    setInterval(() => {
-      fsx.emtyDir('./.cache')
-        .then(() => { logger.info('cache clear') })
-        .catch(err => { logger.error(`${err}`) })
-	  }, 43200000)
-  }
-}*/
+if(!(fs.existsSync('./temp'))) {
+  logger.info('temp not found')
+  fs.mkdir('./temp', { recursive: true }, (err) => {
+    if (err) {
+      logger.error(err, 'Failed to create a folder.')
+      process.exit()
+    } else {
+      logger.info('Folder "./temp" berhasil dibuat.')
+    }
+  })
+} else {
+  logger.info('Folder temp found')
+}
 
 if(!(fs.existsSync('./sessions'))) {
   logger.info('sessions not found')
--- plugins/utility/stiker.js
+++ plugins/utility/stiker.js
@@ -1,14 +1,48 @@
 import { downloadMediaMessage } from '@whiskeysockets/baileys'
+import ffmpeg from 'fluent-ffmpeg'
 import MAIN_LOGGER from '../../lib/logger.js'
-import imagemin from 'imagemin'
-import imageminWebp from 'imagemin-webp'
-import gif2webp from 'imagemin-gif2webp'
 import fs from 'fs'
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
 const logger = MAIN_LOGGER.child({ })
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+function rand(min, max){
+	return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+async function toMp3(file){
+	return new Promise((resolve, reject) => {
+    const outputPath = join(__dirname, `../../temp/${rand(1000000, 99999999)}.webp`);
+    ffmpeg(join(__dirname, `../../temp/${file}`))
+    	.addOutputOptions([
+        `-vcodec`, `libwebp`, `-vf`,
+        `scale='min(320,iw)':min'(320,ih)':force_original_aspect_ratio=decrease,fps=15, pad=320:320:-1:-1:color=white@0.0, split [a][b]; [a] palettegen=reserve_transparent=on:transparency_color=ffffff [p]; [b][p] paletteuse`
+      ])
+      .toFormat('webp')
+      .save(outputPath)
+      .on('error', (err) => reject(`Error converting file: ${err}`))
+      .on('end', () => resolve(outputPath));
+  });
+}
+
+async function cleanupFiles() {
+  try {
+  	const files = fs.readdirSync(join(__dirname, '../../temp'));
+    for (const file of files) {
+    	fs.unlinkSync(join(__dirname, '../../temp', file));
+    }
+  } catch (err) {
+    console.error('Error cleaning up files:', err);
+  }
+}
 
 
 const handler = async (conn, { user, id }, m) => {
   const messageType = Object.keys (m.message)[0]
+  const gif = Object.keys (m.message?.videoMessage || { text: 'undifined' })
   console.log(messageType)
   if (messageType === 'imageMessage') {
     const buffer = await downloadMediaMessage(
@@ -19,21 +53,24 @@ const handler = async (conn, { user, id }, m) => {
         logger,
       }
     )
-    const data = await imagemin.buffer(buffer, {
-		  plugins: [
-		    imageminWebp({
-		      quality: 70,
-		      resize: {
-		        height: 512,
-		        width: 512
-		      },
-		      size: 512
-		    })
-		  ]
+    fs.writeFile(join(__dirname, '../../temp/gambar.jpg'), buffer, async (err) => {
+      if (err){
+        await conn.sendMessage(id, { text: `${err}` })
+      } else {
+        try {
+          const data = await toMp3('gambar.jpg')
+          await conn.sendMessage(id, { sticker: {
+            url: data
+          }})
+        } catch (e) {
+          await conn.sendMessage(id, { text: `${e}` })
+        } finally {
+          await cleanupFiles()
+        }
+      }
     })
-    await conn.sendMessage(id, { sticker: data })
-  } /*else if(messageType === 'videoMessage') {
-		const bufferv = await downloadMediaMessage(
+  } else if(messageType === 'videoMessage' && gif.includes('gifAttribution')) {
+		const buffer = await downloadMediaMessage(
       m,
       'buffer',
       { },
@@ -41,16 +78,24 @@ const handler = async (conn, { user, id }, m) => {
         logger,
       }
     )
-    const datav = await imagemin.buffer(bufferv, {
-      plugins: [
-        gif2webp({
-					quality: 70
-        })
-      ]
-    })
-    await conn.sendMessage(id, { sticker: datav })
-  }*/ else {
-    await conn.sendMessage(id, { text: 'Image Not Found' })
+		fs.writeFile(join(__dirname, '../../temp/gif.mp4'), buffer, async (err) => {
+			if (err){
+				await conn.sendMessage(id, { text: `${err}` })
+			} else {
+				try {
+					const data = await toMp3('gif.mp4')
+					await conn.sendMessage(id, { sticker: {
+						url: data
+					}})
+				} catch (e) {
+					await conn.sendMessage(id, { text: `${e}` })
+				} finally {
+					await cleanupFiles()
+				}
+			}
+		})
+  } else {
+    await conn.sendMessage(id, { text: 'Image Or GIF Not Found' })
   }
 }
 
