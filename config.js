ec9937436a300e9bd8fa33ccb189d7569c349efc
code-seven123 <manu024595@gmail.com>
2024-03-01 14:42:38 +0700

    add env

---
+++ config.js
@@ -0,0 +1,27 @@
+import { config } from 'dotenv';
+config()
+const env = process.env
+const cfg = {}
+//owner
+cfg.prefix = env.BOT_PREFIX
+cfg.botName = env.BOT_NAME
+cfg.owner = (JSON.parse(env.OWNER)).data
+//log
+cfg.levelLog = env.LEVEL_LOG
+
+//menu
+cfg.thumnailMenus = env.THUMNAIL_MENU
+
+//loading
+cfg.loadingMsg = env.LOADING_MSG
+cfg.loadingMsgText = env.LOADING_TEXT
+
+//Browser use
+cfg.browser = env.BROWSER.split(',')
+
+//server web
+cfg.port = env.PORT
+
+console.log(cfg)
+
+export default cfg
