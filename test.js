b9dace51d23643ebb4f0ccd8cd0bb51bb1ac98e1
Code-seven123 <manu024595@gmail.com>
2024-04-07 18:55:36 +0700

    fixed

--- test.js
+++
@@ -1,9 +0,0 @@
-const data = [
-  {                                                                 ytid: [                                                           'irzvPo6f1Gs', '3ba-ZOhf73U',                                   '_r8k7tXvDuQ', 'MgzDyYxOwN8',
-      'Vsie2d6h-e8', 'hkTOOYnQ5rE',                                   'bsRjwww3ofI', 'rgt2nvmpLmg',                                   'AJi2_zSTQrY', 'k7A8hD16hMA',
-      'LB5l5UNpvqw', 'EMSowOKzrXI',
-      '4FkMhdd1OiQ', 'nIx7h36rKu0',
-      'y3JcRIfX5Ao', 'zMYkaK5-Ff8',                                   'MnT3qUT_hUY', 'RXjIZHqDI9A',                                   '98MGT9JStKk', 'aUVGwTpyoe0'
-    ]                                                             }                                                             ]
-
-console.log(data.find(item => item.ytid).ytid[0])
