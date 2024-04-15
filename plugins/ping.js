5ecaa2c96cf1bcd8c91ba0a4ea4631ebea2011de
Code-seven123 <manu024595@gmail.com>
2024-04-15 19:32:01 +0700

    update ytdl

--- plugins/ping.js
+++ plugins/ping.js
@@ -18,23 +18,13 @@ const handler = async (conn, { user, id }, m) => {
     + `🔴Ram: ${toGb(os.totalmem())} / ${toGb(os.totalmem() - os.freemem())}\n`
     + `🟢Free Ram: ${toGb(os.freemem())}\n`
     + `💻Os: ${os.platform() + ' ' + os.arch()}`
-/*  const adReply = {
+  const adReply = {
     text: txt,
     contextInfo: {
       externalAdReply: {
         title: `${frontText(botName).split('').join('.')} : Konnichiwa ${user}`,
         thumbnail: fs.readFileSync(join(__dirname, '../src/icon.jpg')),
         sourceUrl: "https://wa.me/6288222358226"
-      }
-    }
-  }*/
-  const adReply = {
-    text: "Selamat Hari Raya Idul Fitri 1445 H. Dengan berakhirnya bulan suci Ramadhan semoga kita selalu dalam keadaan hati yang bersih dan saling memaafkan satu sama lain.",
-    contextInfo: {
-      externalAdReply: {
-        title: `Idul Fitri 1445H`,
-        thumbnail: fs.readFileSync(join(__dirname, '../src/gambar.jpg')),
-        sourceUrl: "https://nasional.kompas.com/read/2024/04/09/16062411/kemenag-prediksi-idul-fitri-1445-h-jatuh-pada-rabu-besok"
       }
     }
   }
