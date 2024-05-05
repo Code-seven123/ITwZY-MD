9bbf28cc12860b3c44b71431987c2392237a6f45
Code-seven123 <manu024595@gamil.com>
2024-05-05 16:11:08 +0700

    update hidetag

--- plugins/hidetag.js
+++ plugins/hidetag.js
@@ -1,10 +1,16 @@
-const handler = async (conn, { id, reply }, m) => {
+const handler = async (conn, { id, reply, storage }, m) => {
+  let msg = null
+  if(reply?.remoteJid !== undefined){
+    msg = storage.messages[reply.remoteJid].get(reply?.id)
+  } else {
+    msg = m
+  }
   const { participants } = await conn.groupMetadata(id) || id
   const userId = []
   for( const all of participants ){
     userId.push(all.id)
   }
-  await conn.sendMessage(id, { text: "`@everyone`", mentions: userId }, { quoted: m })
+  await conn.sendMessage(id, { text: "`@everyone`", mentions: userId }, { quoted: msg || m })
 }
 
 handler.cmd = /^(hidetag)$/i
