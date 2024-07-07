7a47f36e985abdd6fb20d9bb02206b890f1afb4d
Code-seven123 <manu024595@gamil.com>
2024-07-07 14:59:06 +0700

    add mysql-baileys

---
+++ test.sh
@@ -0,0 +1,9 @@
+curl https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=AIzaSyAQ2WKeobfDRz2r2h0Q8Y4xCkR3GRCnadc \
+-H 'Content-Type: application/json' \
+-d '{
+"contents":[{
+"parts":[
+{"text": "Hello apa kabar?"}
+]}
+]
+}'
