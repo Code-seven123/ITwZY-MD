b966bf256d83c05c5f755ed3912fc6bd0a9c1baf
-Seven <121334040+Code-seven123@users.noreply.github.com>
2024-07-13 18:47:17 +0700

    Update kiryuusearch.js

--- plugins/kiryuusearch.js
+++ plugins/kiryuusearch.js
@@ -3,16 +3,19 @@ import { KiryuuSearch } from "dhn-api"
 const handler = async (conn, { id, args }, m) => {
   if(args == []) return
   const data = await KiryuuSearch(args.join(" "))
-  console.log(data)
-  const txt = data.map(i => `Status: ${i?.status}\n`
-    + `Creator: ${i?.creator}\n`
-    + `Title: ${i?.manga_name?.replaceAll('\t', '')}\t.R ${i?.manga_rating}\n`
-    + `Latest chapter: ${i?.manga_eps}\n`
-    + `URL: ${i?.manga_url}\n`
-  )
-  await conn.sendMessage(id, { 
-    text: `Getting ${data?.length} komik.\n\n`+txt?.join("\n"),
-  }, { quoted: m })
+  if(Array.isArray(data)) {
+    const txt = data.map(i => `Status: ${i?.status}\n`
+      + `Creator: ${i?.creator}\n`
+      + `Title: ${i?.manga_name?.replaceAll('\t', '')}\t.R ${i?.manga_rating}\n`
+      + `Latest chapter: ${i?.manga_eps}\n`
+      + `URL: ${i?.manga_url}\n`
+    )
+    await conn.sendMessage(id, { 
+      text: `Getting ${data?.length} komik.\n\n`+txt?.join("\n"),
+    }, { quoted: m })
+  } else {
+    await conn.sendMessage(id, { text: `Failed search data: \n${data}` })
+  }
   
 }
 
