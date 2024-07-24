e2acd6aadb70e42beabc68540c9248aa92dfc69c
-Seven <121334040+Code-seven123@users.noreply.github.com>
2024-07-24 08:43:13 +0700

    Update kiryuusearch.js

--- plugins/kiryuusearch.js
+++ plugins/kiryuusearch.js
@@ -4,12 +4,12 @@ const handler = async (conn, { id, args }, m) => {
   if(args == []) return
   const data = await KiryuuSearch(args.join(" "))
   if(Array.isArray(data)) {
-    const txt = data.map(i => `Status: ${i?.status}\n`
+    const txt = data?.map(i => `Status: ${i?.status}\n`
       + `Creator: ${i?.creator}\n`
       + `Title: ${i?.manga_name?.replaceAll('\t', '')}\t.R ${i?.manga_rating}\n`
       + `Latest chapter: ${i?.manga_eps}\n`
       + `URL: ${i?.manga_url}\n`
-    )
+    ) || []
     await conn.sendMessage(id, { 
       text: `Getting ${data?.length} komik.\n\n`+txt?.join("\n"),
     }, { quoted: m })
